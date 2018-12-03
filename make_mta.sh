079c8d2bbb021972fd0cb3daf09f46b7cd635ace
gavintfn <gavintfn@users.noreply.github.com>
2018-12-03 06:59:44 -0500

    Update make_mta.sh

--- make_mta.sh
+++ make_mta.sh
@@ -43,12 +43,12 @@ setcap 'cap_net_bind_service=+ep' /usr/bin/ruby2.3
 #
 # Application Setup
 #
-sudo -i -u postal mkdir -p /opt/postal/app
+#sudo -i -u postal mkdir -p /opt/postal/app
 wget https://postal.atech.media/packages/stable/latest.tgz -O - | sudo -u postal tar zxpv -C /opt/postal/app
-rm /opt/postal/spec/config/postal.yml
+rm /opt/postal/app/spec/config/postal.yml
 wget https://raw.githubusercontent.com/gavintfn/postal/master/spec/config/postal.yml
-cp postal.yml /opt/postal/spec/config/postal.yml
-chown postal:postal /opt/postal/spec/config/postal.yml
+cp postal.yml /opt/postal/app/spec/config/postal.yml
+chown postal:postal /opt/postal/app/spec/config/postal.yml
 chown postal:postal /opt/postal/ -R
 #cd /opt/postal
 #sudo -i -u postal wget https://github.com/gavintfn/postal/archive/master.zip
