5beb21a2176bbab29305b93dc7da234662424b4b
gavintfn <gavintfn@users.noreply.github.com>
2018-12-15 10:43:53 -0500

    Update finish_letsencrypt.sh

--- finish_letsencrypt.sh
+++ finish_letsencrypt.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e
-
-acme.sh --issue -d postal.business-perks.com -w /opt/postal/app/public/
-sudo /usr/bin/perl -pi -e -i 's/ssl_certificate/#ssl_certificate/g'  /etc/nginx/sites-enabled/default
-sudo /usr/bin/perl -pi -e -i "s/ssl_certificate_key/#ssl_certificate_key/g"  /etc/nginx/sites-enabled/default
-sudo /usr/bin/perl -pi -e -i "s/#certificate/ssl_certificate/g"  /etc/nginx/sites-enabled/default
-sudo /usr/bin/perl -pi -e -i "s/#certificate_key/ssl_certificate_key/g"  /etc/nginx/sites-enabled/default
-systemctl nginx restart
-acme.sh --install-cert -d XXXFQDNXXX --cert-file /etc/nginx/certs/XXXFQDNXXX/cert --key-file /etc/nginx/certs/XXXFQDNXXX/key --fullchain-file /etc/nginx/certs/XXXFQDNXXX/fullchain --reloadcmd "systemctl reload nginx.service"
+/root/.acme.sh/acme.sh --issue -d XXXFQDNXXX -w /opt/postal/app/public/
+/usr/bin/perl -pi -e -i 's/ssl_certificate/#ssl_certificate/g'  /etc/nginx/sites-enabled/default
+/usr/bin/perl -pi -e -i "s/ssl_certificate_key/#ssl_certificate_key/g"  /etc/nginx/sites-enabled/default
+/usr/bin/perl -pi -e -i "s/#certificate/ssl_certificate/g"  /etc/nginx/sites-enabled/default
+/usr/bin/perl -pi -e -i "s/#certificate_key/ssl_certificate_key/g"  /etc/nginx/sites-enabled/default
+/root/.acme.sh/acme.sh --install-cert -d XXXFQDNXXX --cert-file /etc/nginx/certs/XXXFQDNXXX/cert --key-file /etc/nginx/certs/XXXFQDNXXX/key --fullchain-file /etc/nginx/certs/XXXFQDNXXX/fullchain --reloadcmd "systemctl reload nginx.service"
