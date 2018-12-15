fe3229e425804dd0cbe8bf2754f08d6a89726ea4
gavintfn <gavintfn@users.noreply.github.com>
2018-12-15 11:59:11 -0500

    Update finish_letsencrypt.sh

--- finish_letsencrypt.sh
+++ finish_letsencrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /root/.acme.sh/acme.sh --issue -d XXXFQDNXXX -w /opt/postal/app/public/
-/usr/bin/perl -pi -e -i 's/ssl_certificate/#ssl_certificate/g'  /etc/nginx/sites-enabled/default
+/usr/bin/perl -pi -e -i "s/ssl_certificate/#ssl_certificate/g"  /etc/nginx/sites-enabled/default
 /usr/bin/perl -pi -e -i "s/ssl_certificate_key/#ssl_certificate_key/g"  /etc/nginx/sites-enabled/default
 /usr/bin/perl -pi -e -i "s/#certificate/ssl_certificate/g"  /etc/nginx/sites-enabled/default
 /usr/bin/perl -pi -e -i "s/#certificate_key/ssl_certificate_key/g"  /etc/nginx/sites-enabled/default
